f5c227ae2234b3a927022a6c03aa196652fd4cdf
Navdeep Singh Brar <123911801+nbrar1282@users.noreply.github.com>
2025-03-29 15:42:42 -0700

    Create cicdPipeline.groovy

---
+++ vars/cicdPipeline.groovy
@@ -0,0 +1,54 @@
+def call(Map config) {
+    pipeline {
+        agent any
+        stages {
+            stage('Lint') {
+                steps {
+                    dir("${config.serviceDir}") {
+                        sh 'pip install pylint'
+                        sh 'pylint *.py --fail-under=5'
+                    }
+                }
+            }
+
+            stage('Security') {
+                steps {
+                    dir("${config.serviceDir}") {
+                        sh 'pip install bandit'
+                        sh 'bandit -r .'
+                    }
+                }
+            }
+
+            stage('Package') {
+                steps {
+                    dir("${config.serviceDir}") {
+                        script {
+                            dockerImage = docker.build("${config.imageName}")
+                            docker.withRegistry('https://index.docker.io/v1/', 'dockerhub-creds') {
+                                dockerImage.push("${config.tag}")
+                            }
+                        }
+                    }
+                }
+            }
+
+            stage('Deploy') {
+              when {
+                expression { return params.DEPLOY == true }
+              }
+              steps {
+                sshagent(['ssh-to-3855vm']) {
+                  sh """
+                  ansible-playbook -i ~/ansible/inventory.yaml ~/ansible/deploy_project.yml
+                  """
+                }
+              }
+            }
+        }
+
+        parameters {
+            booleanParam(name: 'DEPLOY', defaultValue: false, description: 'Trigger Deploy Stage Manually')
+        }
+    }
+}
