5b75f37253a2189d6e7c5fa66d4ba9b553b66034
Navdeep Singh Brar <123911801+nbrar1282@users.noreply.github.com>
2025-03-29 17:17:24 -0700

    Update cicdPipeline.groovy

--- vars/cicdPipeline.groovy
+++ vars/cicdPipeline.groovy
@@ -13,8 +13,8 @@ def call(Map config) {
             stage('Security') {
                 steps {
                     dir("${config.serviceDir}") {
-                        sh 'pip install bandit'
-                        sh 'bandit -r .'
+                        sh './venv/bin/pip install bandit'
+                        sh './venv/bin/bandit -r .'
                     }
                 }
             }
