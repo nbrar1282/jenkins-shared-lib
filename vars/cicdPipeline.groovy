1be44323eb95ff35aa7c70ad11ae6e2c351498e7
Navdeep Singh Brar <123911801+nbrar1282@users.noreply.github.com>
2025-03-29 17:11:23 -0700

    Update cicdPipeline.groovy

--- vars/cicdPipeline.groovy
+++ vars/cicdPipeline.groovy
@@ -1,18 +1,11 @@
 def call(Map config) {
     pipeline {
-        agent { label 'python_agent' }
-
+        agent { 'python_agent' }
         stages {
             stage('Lint') {
                 steps {
                     dir("${config.serviceDir}") {
-                        sh '''
-                            python3 -m venv venv
-                            source venv/bin/activate
-                            pip install --upgrade pip
-                            pip install pylint
-                            venv/bin/pylint *.py --fail-under=1
-                        '''
+                        sh 'pylint *.py --fail-under=1'
                     }
                 }
             }
@@ -20,17 +13,13 @@ def call(Map config) {
             stage('Security') {
                 steps {
                     dir("${config.serviceDir}") {
-                        sh '''
-                            python3 -m venv venv
-                            source venv/bin/activate
-                            pip install --upgrade pip
-                            pip install bandit
-                            venv/bin/bandit -r .
-                        '''
+                        sh 'pip install bandit'
+                        sh 'bandit -r .'
                     }
                 }
             }
 
+            
             stage('Package') {
                 when {
                     expression { env.GIT_BRANCH == 'origin/main' }
@@ -38,27 +27,25 @@ def call(Map config) {
                 steps {
                     dir("${config.serviceDir}") {
                         withCredentials([string(credentialsId: 'DockerHub', variable: 'TOKEN')]) {
-                            sh '''
-                                echo "$TOKEN" | docker login -u "nbrar1282" --password-stdin
-                                docker build -t ${config.imageName}:latest --tag ${config.imageName}:${config.tag} .
-                                docker push ${config.imageName}:${config.tag}
-                            '''
+                            sh "docker login -u 'nbrar1282' -p '$TOKEN' docker.io"
+                            sh "docker build -t ${config.imageName}:latest --tag ${config.imageName}:${config.tag} ."
+                            sh "docker push ${config.imageName}:${config.tag}"
                         }
                     }
                 }
             }
 
             stage('Deploy') {
-                when {
-                    expression { return params.DEPLOY == true }
+              when {
+                expression { return params.DEPLOY == true }
+              }
+              steps {
+                sshagent(['ssh-to-3855vm']) {
+                  sh """
+                  ansible-playbook -i ~/ansible/inventory.yaml ~/ansible/deploy_project.yml
+                  """
                 }
-                steps {
-                    sshagent(['ssh-to-3855vm']) {
-                        sh '''
-                            ansible-playbook -i ~/ansible/inventory.yaml ~/ansible/deploy_project.yml
-                        '''
-                    }
-                }
+              }
             }
         }
 
